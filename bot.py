98c7aac947cc7f80bdaae5a1e16673852ebbb19d
kabekaaa-pixel <kabekaaa@gmail.com>
2026-02-20 00:40:43 +0100

    updatik bot.py
    обнова епта

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import random
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pytz
 from aiogram import Bot, Dispatcher, types, F
@@ -15,8 +15,8 @@ from aiogram.types import (
 
 TOKEN = "8279523638:AAFQoHMem4XCW2eq3fdcC0nmMbHysJsrED4"
 
-HER_ID = 2007593176   # ВСТАВЬ ЕЁ ID
-MY_ID = 1395307876    # ВСТАВЬ СВОЙ ID
+HER_ID = 2007593176
+MY_ID = 1395307876
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -44,27 +44,40 @@ MESSAGES = [
     "я тебя безумно люблю. это навсегда, я уверен!! 143",
     "светись также ярко как и сейчас, моя любимая. 143",
     "самой любименькой на свете аришке хочу сказать что я её очень сильно люблю и обожаю!!! 143",
-    "ялюблютебятаксильнотыпростонеможешьпредставитьадумаюотебекаждуюсекунду"
+    "ялюблютебятаксильнотыпростонеможешьпредставитьадумаюотебекаждуюсекунду",
+    "самой красивой и шикарной девушке хочу сказать я очень сильно её ценю и обожаю, как мне с ней повезло и как же она красива и прекрасна… 143",
+    "я не могу поверить что ты реальная, неужели такая идеальная девушка как ты действительно существует… я никогда не видел таких красивых девочек… не я серьезно… 143…",
+    "Я ТЕБЯ ПРОСТО ОБОЖАЮ МОЯ ХОРОШЕНЬКАЯ!!! Я ОЧЕНЬ ХОЧУ ТЕБЯ ОБНИМАТЬ И ЦЕЛОВАТЬ!!! ОЧЕНЬ ТЕБЯ ЛЮБЛЮ!! 143!!",
+    "ты у меня самая любименькая красивая и прекрасная!! я очень тебя люблю ариша!!!!! 143",
+    "я не верю что ты можешь быть такой красивой(( может я просто сплю? ты безусловно заслужила эту красоту, но я не верю, что это просто возможно ваще… 143"
 ]
 
-MONOLOG = """«солнце мое я уже стал путаться я хоть когда-то вообще не думаю о тебе или как? я не знаю как описать эти чувства не то что текстом, я не уверен что я даже тактильно смогу показать то насколько тебя люблю. это что-то большее чем просто чувства. я не знаю просто что это… я с каждым днем только чаще думаю о том, какая ты хорошая и о том как я тебя люблю.. а особенно о том, какая ты красивая, у меня по ощущению одно полушарие полностью этими мыслями занято ахахаха. я просто не знаю даже как вообще описать какой я тебя вижу. просто, идеальной? все равно не полностью описано. ну типа, я восхищаюсь каждой частичкой твоей внешности: твоими красивыми глазами, ресницами, твоими шелковыми волосами, красивыми губами, носику, но главное конечно, что из этого получается просто до безумия красивое и милое лицо. я готов смотреть на тебя вечность, правда. у тебя самая милая улыбка. прям ну очень. а если ты и не улыбаешься, то ты по прежнему остаешься самой красивой на свете. но лучше конечно, чтобы ты улыбалась. если ты счастлива, то и я счастлив. ты лучшее что со мной случалось, сколько бы я раз об этом не говорил. моя любовь к тебе просто бесконечна. я очень тебя люблю, моя хорошая. обожаю всем сердцем. еще раз, ты самая красивая, милая, добрая, смешная, и всеми хорошими качествами которые я только могу перечислить, ты обладаешь на максимум. люблю тебя безумно, даже если ты и не забывала это, все равно напомню. ты у меня самая любименькая. от серёжки. 143»"""  # вставь свой полный текст
+MONOLOG = "солнце мое я уже стал путаться я хоть когда-то вообще не думаю о тебе или как? я не знаю как описать эти чувства не то что текстом, я не уверен что я даже тактильно смогу показать то насколько тебя люблю. это что-то большее чем просто чувства. я не знаю просто что это… я с каждым днем только чаще думаю о том, какая ты хорошая и о том как я тебя люблю.. а особенно о том, какая ты красивая, у меня по ощущению одно полушарие полностью этими мыслями занято ахахаха. я просто не знаю даже как вообще описать какой я тебя вижу. просто, идеальной? все равно не полностью описано. ну типа, я восхищаюсь каждой частичкой твоей внешности: твоими красивыми глазами, ресницами, твоими шелковыми волосами, красивыми губами, носику, но главное конечно, что из этого получается просто до безумия красивое и милое лицо. я готов смотреть на тебя вечность, правда. у тебя самая милая улыбка. прям ну очень. а если ты и не улыбаешься, то ты по прежнему остаешься самой красивой на свете. но лучше конечно, чтобы ты улыбалась. если ты счастлива, то и я счастлив. ты лучшее что со мной случалось, сколько бы я раз об этом не говорил. моя любовь к тебе просто бесконечна. я очень тебя люблю, моя хорошая. обожаю всем сердцем. еще раз, ты самая красивая, милая, добрая, смешная, и всеми хорошими качествами которые я только могу перечислить, ты обладаешь на максимум. люблю тебя безумно, даже если ты и не забывала это, все равно напомню. ты у меня самая любименькая. от серёжки. 143"
 
-# ---------- Работа с файлом ----------
+# -------------------- DATA --------------------
 
 def load_data():
     try:
         with open(DATA_FILE, "r") as f:
             return json.load(f)
     except:
-        return {"chat_id": None, "reminders_enabled": False}
+        return {
+            str(MY_ID): {"enabled": False, "waiting_send": False},
+            str(HER_ID): {"enabled": False, "waiting_send": False}
+        }
 
-def save_data(data):
+def save_data():
     with open(DATA_FILE, "w") as f:
         json.dump(data, f)
 
 data = load_data()
 
-# ---------- Проверка доступа ----------
+def ensure_user(user_id):
+    if str(user_id) not in data:
+        data[str(user_id)] = {"enabled": False, "waiting_send": False}
+        save_data()
+
+# -------------------- ACCESS --------------------
 
 def is_allowed(user_id):
     return user_id in [HER_ID, MY_ID]
@@ -72,20 +85,32 @@ def is_allowed(user_id):
 def other_user(user_id):
     return HER_ID if user_id == MY_ID else MY_ID
 
-# ---------- Клавиатура ----------
+# -------------------- KEYBOARDS --------------------
 
-def reply_keyboard():
-    reminder_text = "выключить напоминания" if data["reminders_enabled"] else "включить напоминания"
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="обнять"), KeyboardButton(text="поцеловать")],
-            [KeyboardButton(text=reminder_text)]
-        ],
-        resize_keyboard=True
-    )
+def reply_keyboard(user_id):
+    ensure_user(user_id)
 
-def inline_keyboard():
-    if data["reminders_enabled"]:
+    enabled = data[str(user_id)]["enabled"]
+    waiting = data[str(user_id)]["waiting_send"]
+
+    reminder_text = "выключить напоминания" if enabled else "включить напоминания"
+
+    keyboard = [
+        [KeyboardButton(text="обнять"), KeyboardButton(text="поцеловать")],
+        [KeyboardButton(text=reminder_text)]
+    ]
+
+    if user_id == MY_ID:
+        if waiting:
+            keyboard.append([KeyboardButton(text="отмена")])
+        else:
+            keyboard.append([KeyboardButton(text="сообщение")])
+
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+
+def inline_keyboard(user_id):
+    enabled = data[str(user_id)]["enabled"]
+    if enabled:
         return InlineKeyboardMarkup(
             inline_keyboard=[[InlineKeyboardButton(text="выключить напоминания", callback_data="stop")]]
         )
@@ -94,68 +119,134 @@ def inline_keyboard():
             inline_keyboard=[[InlineKeyboardButton(text="включить напоминания", callback_data="enable")]]
         )
 
-# ---------- /start ----------
+# -------------------- START --------------------
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
     if not is_allowed(message.from_user.id):
         return
-    data["chat_id"] = message.chat.id
-    save_data(data)
-    await message.answer("ну шо ты косолапая", reply_markup=reply_keyboard())
-    await message.answer(" ", reply_markup=inline_keyboard())
 
-# ---------- Inline ----------
+    ensure_user(message.from_user.id)
+
+    await message.answer(
+        "ну шо ты косолапая",
+        reply_markup=reply_keyboard(message.from_user.id)
+    )
+
+    await message.answer(
+        " ",
+        reply_markup=inline_keyboard(message.from_user.id)
+    )
+
+# -------------------- ENABLE/DISABLE --------------------
 
 @dp.callback_query(F.data == "enable")
 async def enable(callback: types.CallbackQuery):
     if not is_allowed(callback.from_user.id):
         return
-    data["reminders_enabled"] = True
-    save_data(data)
+
+    user_id = callback.from_user.id
+    ensure_user(user_id)
+
+    data[str(user_id)]["enabled"] = True
+    save_data()
+
     await callback.message.answer(
         "уряяя!! 143143143!!!! теперь ты точно никогда не забудешь о том как я тебя люблю!",
-        reply_markup=reply_keyboard()
+        reply_markup=reply_keyboard(user_id)
     )
+
+    await callback.message.answer(
+        " ",
+        reply_markup=inline_keyboard(user_id)
+    )
+
     await callback.answer()
 
 @dp.callback_query(F.data == "stop")
 async def stop(callback: types.CallbackQuery):
     if not is_allowed(callback.from_user.id):
         return
-    data["reminders_enabled"] = False
-    save_data(data)
-    await callback.message.answer("ну блин, я понимаю что ты это и так знаешь, но всё же( ну лан, надеюсь тебе понравилось!")
-    await callback.answer()
 
-# ---------- Текстовые кнопки ----------
+    user_id = callback.from_user.id
+    ensure_user(user_id)
+
+    data[str(user_id)]["enabled"] = False
+    save_data()
+
+    await callback.message.answer(
+        "ну блин, я понимаю что ты это и так знаешь, но всё же..( ну лан, надеюсь тебе понравилось!",
+        reply_markup=reply_keyboard(user_id)
+    )
+
+    await callback.message.answer(
+        " ",
+        reply_markup=inline_keyboard(user_id)
+    )
+
+    await callback.answer()
 
 @dp.message(F.text == "включить напоминания")
 async def enable_text(message: types.Message):
-    if not is_allowed(message.from_user.id):
-        return
-    data["reminders_enabled"] = True
-    save_data(data)
-    await message.answer(
-        "уряяя!! 143143143!!!! теперь ты точно никогда не забудешь о том как я тебя люблю!",
-        reply_markup=reply_keyboard()
-    )
+    await enable(types.CallbackQuery(
+        id="text",
+        from_user=message.from_user,
+        chat_instance="",
+        message=message,
+        data="enable"
+    ))
 
 @dp.message(F.text == "выключить напоминания")
 async def disable_text(message: types.Message):
-    if not is_allowed(message.from_user.id):
+    await stop(types.CallbackQuery(
+        id="text",
+        from_user=message.from_user,
+        chat_instance="",
+        message=message,
+        data="stop"
+    ))
+
+# -------------------- SEND MODE --------------------
+
+@dp.message(Command("send"))
+@dp.message(F.text == "сообщение")
+async def enter_send(message: types.Message):
+    if message.from_user.id != MY_ID:
         return
-    data["reminders_enabled"] = False
-    save_data(data)
-    await message.answer("ну блин, я понимаю что ты это и так знаешь, но всё же( ну лан, надеюсь тебе понравилось!")
-    
-# ---------- Hug / Kiss ----------
+
+    data[str(MY_ID)]["waiting_send"] = True
+    save_data()
+
+    await message.answer(
+        "напиши чонить",
+        reply_markup=reply_keyboard(MY_ID)
+    )
+
+@dp.message(F.text == "отмена")
+@dp.message(Command("cancel"))
+async def cancel_send(message: types.Message):
+    if message.from_user.id != MY_ID:
+        return
+
+    data[str(MY_ID)]["waiting_send"] = False
+    save_data()
+
+    await message.answer(
+        "откат",
+        reply_markup=reply_keyboard(MY_ID)
+    )
+
+# -------------------- HUG / KISS --------------------
 
 @dp.message(Command("hug"))
 @dp.message(F.text == "обнять")
 async def hug(message: types.Message):
     if not is_allowed(message.from_user.id):
         return
+
+    if data[str(message.from_user.id)]["waiting_send"]:
+        return
+
     await message.answer("обнимашки переданы!!")
     await bot.send_message(other_user(message.from_user.id), "тебя обняли!!")
 
@@ -164,14 +255,29 @@ async def hug(message: types.Message):
 async def kiss(message: types.Message):
     if not is_allowed(message.from_user.id):
         return
+
+    if data[str(message.from_user.id)]["waiting_send"]:
+        return
+
     await message.answer("поцелуй передан!")
     await bot.send_message(other_user(message.from_user.id), "тебя поцеловали!")
 
-# ---------- Строгие триггеры ----------
+# -------------------- TEXT HANDLER --------------------
 
 @dp.message(F.text)
-async def strict_triggers(message: types.Message):
+async def text_handler(message: types.Message):
     if not is_allowed(message.from_user.id):
+        return
+
+    user_id = message.from_user.id
+    ensure_user(user_id)
+
+    # если режим send
+    if user_id == MY_ID and data[str(MY_ID)]["waiting_send"]:
+        await bot.send_message(HER_ID, message.text)
+        data[str(MY_ID)]["waiting_send"] = False
+        save_data()
+        await message.answer("отправил", reply_markup=reply_keyboard(MY_ID))
         return
 
     text = message.text
@@ -185,61 +291,49 @@ async def strict_triggers(message: types.Message):
     elif text == "я люблю тебя":
         await message.answer("и я тебя")
 
-    elif text == "я люблю сережу" or text == "я люблю серёжу":
+    elif text in ["я люблю сережу", "я люблю серёжу"]:
         await message.answer("а я люблю аришу")
 
-# ---------- Scheduler ----------
+# -------------------- SCHEDULER --------------------
 
-last_sent = None
+last_sent = {}
 
 async def reminder_loop():
-    global last_sent
     await asyncio.sleep(5)
 
     while True:
-        if not data["reminders_enabled"] or not data["chat_id"]:
-            await asyncio.sleep(30)
-            continue
-
         now = datetime.now(OMSK_TZ)
         key = f"{now.hour}:{now.minute}"
 
-        # 6:24 и 1:43
-        if now.minute in [24, 43] and key != last_sent:
+        for user_id in [MY_ID, HER_ID]:
+            ensure_user(user_id)
+
+            if not data[str(user_id)]["enabled"]:
+                continue
+
+            if last_sent.get((user_id, key)):
+                continue
+
+            # 6:24 и 1:43
             if (now.hour == 6 and now.minute == 24) or (now.hour == 1 and now.minute == 43):
-                text = random.choice(MESSAGES)
-                await bot.send_message(data["chat_id"], text)
-                last_sent = key
+                await bot.send_message(user_id, random.choice(MESSAGES))
+                last_sent[(user_id, key)] = True
 
-        # 14:43 — 10%
-        if now.hour == 14 and now.minute == 43 and key != last_sent:
-            if random.random() <= 0.10:
-                await bot.send_message(data["chat_id"], "💋")
-            last_sent = key
+            # 14:43 — 10%
+            if now.hour == 14 and now.minute == 43:
+                if random.random() <= 0.10:
+                    await bot.send_message(user_id, "💋")
+                last_sent[(user_id, key)] = True
 
-        # Нечётные часы — 1%
-        if now.minute == 43 and now.hour % 2 == 1 and key != last_sent:
-            if random.random() <= 0.01:
-                await bot.send_message(data["chat_id"], MONOLOG)
-            last_sent = key
+            # Нечётные часы — 1%
+            if now.minute == 43 and now.hour % 2 == 1:
+                if random.random() <= 0.01:
+                    await bot.send_message(user_id, MONOLOG)
+                last_sent[(user_id, key)] = True
 
         await asyncio.sleep(20)
 
-# ---------- /send для админа ----------
-
-@dp.message(Command("send"))
-async def send_to_her(message: types.Message):
-    if message.from_user.id != MY_ID:
-        return  # Только для твоего ID
-    # Получаем текст после команды /send
-    text = message.get_args()  # Возьмёт всё после "/send "
-    if not text:
-        await message.answer("ну ты текст то напиши")
-        return
-    await bot.send_message(HER_ID, text)
-    await message.answer(f"отправил: {text}")
-
-# ---------- Запуск ----------
+# -------------------- RUN --------------------
 
 async def main():
     asyncio.create_task(reminder_loop())
